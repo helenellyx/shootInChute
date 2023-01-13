84d400a7b6804cc7ab027164890f841e236e42be
Helen Guan <helenguan30@gmail.com>
2023-01-13 16:26:46 -0500

    Fix import error

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { HomeScreen } from './screens/HomeScreen';
 
 const Stack = createNativeStackNavigator();
 
