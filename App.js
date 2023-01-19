59092111ca55e9c95d64b02c55f084aa9b9251bb
Joyce Ye <yejoyce8@gmail.com>
2023-01-18 23:48:15 -0500

    Created tab navigation bar
    Made a bottom tab navigation bar but need to resize the icons and position them in the right place.

--- App.js
+++ App.js
@@ -2,49 +2,82 @@ import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import HomeScreen from './Screens/HomeScreen';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+
+import { Octicons } from '@expo/vector-icons'
+
+function SearchScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+function ProfileScreen() {
+  return(
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Profile!</Text>
+    </View>
+  )
+}
+
+
+
+const Tab = createBottomTabNavigator();
+
+// Tabs menu
+function MyTabs() {
+  return (
+    <Tab.Navigator 
+      screenOptions={({route}) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = 'home';
+          } else if (route.name === 'Search') {
+            iconName = 'search';
+          } else if(route.name ==='Profile'){
+            iconName = 'person';
+          } 
+          color = focused ? '#FFE6A2' : '#FCF7E7';
+
+          // You can return any component that you like here!
+          return <Octicons name={iconName} size={size} color={color} />
+        },
+        
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          position: 'absolute',
+          width: 353,
+          height: 69,
+          left: 20,
+          top: 740,
+          borderRadius: 28,
+          backgroundColor: '#74CCA8',
+        }
+  })} >
+			<Tab.Screen name="Home" component={HomeScreen} />
+			<Tab.Screen name="Search" component={SearchScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+		</Tab.Navigator>
+
+  )
+}
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
+	return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-        />
-        <Stack.Screen name="Search" component={SearchScreen} />
-      </Stack.Navigator>
+      <MyTabs />
+
     </NavigationContainer>
-  );
+		
+
+	);
 }
-
-const HomeScreen= () => {
-  return(
-  <View style={styles.container}>
-      <Text>home screen</Text>
-    </View>
-);
-  }
-
-const SearchScreen= () => {
-  return(
-  <View style={styles.container}>
-      <Text>search screen</Text>
-      <StatusBar style="auto" />
-    </View>
-);
-  }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#FCF7E7',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
-
-
--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,1 +1,10 @@
-//put code for homescreen here
+import * as React from 'react';
+//import { ScrollView } from 'react-native-web';
+import {Text} from 'react-native';
+
+export default function HomeScreen() {
+    return (
+    <Text> this is new home screen!</Text>
+        
+    )
+}
