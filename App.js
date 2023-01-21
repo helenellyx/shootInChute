ac1f8ac645a0c72458db5f39282d4b9f99845429
Helen Guan <helenguan30@gmail.com>
2023-01-21 15:53:17 -0500

    Set up React Native Paper Library

--- App.js
+++ App.js
@@ -2,21 +2,47 @@ import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
+import {
+  MD3LightTheme as DefaultTheme,
+  Provider as PaperProvider,
+} from 'react-native-paper';
 import { StyleSheet, Text, View } from 'react-native';
+import SearchScreen from './src/screens/searchScreen';
 
 const Stack = createNativeStackNavigator();
 
+const theme = {
+  ...DefaultTheme,
+  // Specify custom property
+  myOwnProperty: true,
+  // Specify custom property in nested object
+  elevation: 0,
+  colors: {
+    background: '#FCF7E7',
+    card: '#FFE6A2',
+    outline: '#74CCA8',
+    border: '#397A89',
+    secondary: '#74CCA8',
+    tertiary: '#2A4849',
+  fonts: {
+
+  }
+
+  },
+};
+
+//options={{headerShown: false}} 
 export default function App() {
   return (
-    <NavigationContainer>
+    <PaperProvider theme={theme}>
+    <NavigationContainer theme={theme}>
       <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
+        <Stack.Screen name="Search" component={SearchScreen}/>
+        <Stack.Screen name="Home" component={HomeScreen}
         />
-        <Stack.Screen name="Search" component={SearchScreen} />
       </Stack.Navigator>
     </NavigationContainer>
+    </PaperProvider>
   );
 }
 
@@ -28,14 +54,6 @@ const HomeScreen= () => {
 );
   }
 
-const SearchScreen= () => {
-  return(
-  <View style={styles.container}>
-      <Text>search screen</Text>
-      <StatusBar style="auto" />
-    </View>
-);
-  }
 
 const styles = StyleSheet.create({
   container: {
