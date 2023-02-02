298b9ff6d6ee9c82744753483b87c65f5deeb168
Helen Guan <helenguan30@gmail.com>
2023-02-01 20:58:28 -0500

    Get rid of profileScreen app

--- App.js
+++ App.js
@@ -20,16 +20,6 @@ function SearchScreen({navigation}) {
   );
 }
 
-function ProfileScreen() {
-  return(
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Profile!</Text>
-      
-    </View>
-  )
-}
-
-
 
 const Tab = createBottomTabNavigator();
 
